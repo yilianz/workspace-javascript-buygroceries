e2c1af446e75da58159bfc4b121dd5eefbc266f9
yilianz <yilianz@gmail.com>
2020-11-02 04:09:32 -0500

    1 file modified

--- js/add-element.js
+++ js/add-element.js
@@ -2,17 +2,27 @@
  * Practice on Elements
  */
 
-// get all the hot class elements- chane them to cool
+// get all the hot class elements- change them to cool class
+
 
 // traverse the elements
 
+
 // add a new element
+
 
 // add by clicking the plus sign
 document.getElementById("add").addEventListener("click", addElement);
 
 function addElement() {
   // add a new element
+  
+}
+
+
+//  click the li element will change the changeStyle
+function changeStyle() {
+
 }
 
 // delete by clicking the trash can
@@ -20,4 +30,6 @@ document.getElementById("remove").addEventListener("click", removeElement);
 
 function removeElement() {
   // remove the marked element
+ 
+ 
 }
